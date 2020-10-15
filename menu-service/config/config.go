4bae6bc2d1d585ff39c36f63af6cf8f86562fc0a
agunghimura11 <agung.arisudana@gmail.com>
2020-10-15 12:00:31 +0800

    init db connection

---
+++ menu-service/config/config.go
@@ -0,0 +1,7 @@
+package config
+
+type Config struct {
+	Port string
+	Port     string
+	Database Database
+}
--- menu-service/main.go
+++ menu-service/main.go
@@ -5,15 +5,50 @@ import (
 	"log"
 	"net/http"
 	"github.com/gorilla/mux"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 
 	"digitalent-microservice/menu-service/handler"
 )
 
 func main(){
+
+	cfg := config.Config{
+		Database : config.Database{
+			Driver   : "mysql",
+			Host     : "localhost",
+			Port     : "3306",
+			User     : "root",
+			Password : "",
+			DbName   : "test",
+			Config   :`charset=utf8&parseTime=True&loc=Local`,
+		},
+	}
+	
+	db, err := initDB(cfg.Database)
+	if err != nil {
+		log.Panic(err)
+		return
+	} 
+
 	router := mux.NewRouter()
 
 	router.Handle("/add-menu", http.HandlerFunc(handler.AddMenu))
 
 	fmt.Println("Menu service listen on port :8000")
 	log.Panic(http.ListenAndServe(":8000", router))
+}
+
+func initDB(dbConfig config.Database) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?%s", dbConfig.User, dbConfig.Password, dbConfig.Host, dbConfig.Port, dbConfig.Config)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	db.AutoMigrate(database.Menu{})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
